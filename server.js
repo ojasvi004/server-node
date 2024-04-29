01eb1973bc8a3e2cc0f83972f5ae7bc671d3de03
Ojasvi <ojasvidoye@gmail.com>
2024-04-29 16:02:28 +0530

    first backend commit yay

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises; 
+
+const logEvents = require('./logEvents');
+const EventEmitter = require('events');
+class Emitter extends EventEmitter { };
+
+const myEmitter = new Emitter();
